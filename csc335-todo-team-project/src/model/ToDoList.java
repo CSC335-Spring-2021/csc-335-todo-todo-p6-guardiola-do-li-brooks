a2c08b06976eb313ebb42835307af3a2c559ac40
mauriciobrooks <mauriciobrooks@email.arizona.edu>
2021-04-21 17:38:44 -0700

    initial add/remove

--- csc335-todo-team-project/src/model/ToDoList.java
+++ csc335-todo-team-project/src/model/ToDoList.java
@@ -4,26 +4,38 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Observable;
 
+@SuppressWarnings("deprecation")
 public class ToDoList extends Observable implements Serializable {
-	ArrayList<ToDoTask> tasks;
-	
-	public ToDoList() {
-		// Implement
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1L;
+    private ArrayList<ToDoTask> tasks;
+
+    public ToDoList() {
+	this.tasks = new ArrayList<ToDoTask>();
+    }
+
+    public void addTask(ToDoTask task) {
+	if (task != null) {
+	    this.tasks.add(task);
+	} else {
+	    System.out.println("***TASK ENTERED IS NULL"); // for debugging
 	}
-	
-	public void addTask() {
-		// Implement
+    }
+
+    public void removeTask(ToDoTask task) {
+	boolean taskSeen = this.tasks.remove(task);
+	if (!taskSeen) {
+	    System.out.println("***Task entered is not in the To-Do List"); // for debugging
 	}
-	
-	public void removeTask() {
-		// Implement
-	}
-	
-	public void saveList() {
-		
-	}
-	
-	public void loadList() {
-		
-	}
+    }
+
+    public void saveList() {
+	// TODO: Implement
+    }
+
+    public void loadList() {
+	// TODO: Implement
+    }
 }
